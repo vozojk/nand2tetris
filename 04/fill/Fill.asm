daa25d4be533584f3a3ee24a4e9f7131c9bcb18a
Vojtěch Šandera <v.sandera@student.tudelft.nl>
2025-12-06 00:29:54 +0100

    Made a loop to turn screen black

---
+++ 04/fill/Fill.asm
@@ -0,0 +1,27 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input. 
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel. When no key is pressed, 
+// the screen should be cleared.
+
+//// Replace this comment with your code.
+
+//turn screen black loop
+@SCREEN
+D=A
+@n
+M=D
+(LOOP)
+@n
+A=M
+M=-1
+@n
+M=M+1
+@LOOP
+0;JMP
+
+
